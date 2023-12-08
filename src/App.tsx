f348d46254eadd62ffcd4c0e86a5bb35c1d6e1ea
erikvieczorek <erikvieczorek@gmail.com>
2023-12-08 08:06:56 -0300

    excluindo arquivos css e ultimas mudanças

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { ThemeProvider } from 'styled-components'
+
+import EstiloGlobal, { Container } from './styles'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import temaLight from './themes/light'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaLight}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,16 @@
-import styles from './Hero.module.css'
+import { Form, FormDiv, HeroTitle } from './styles'
+import { Container } from '../../styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <Form>
+    <Container>
+      <FormDiv>
+        <HeroTitle>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </HeroTitle>
+      </FormDiv>
+    </Container>
+  </Form>
 )
 
 export default Hero
